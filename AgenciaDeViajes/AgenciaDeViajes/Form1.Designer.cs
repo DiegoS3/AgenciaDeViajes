6c835a8d82cf9c3a0434ac125aabfeff37ddaaf4
DiegoS3 <diegozj92@gmail.com>
2020-10-16 14:15:20 +0200

    Commit Final Splash

--- AgenciaDeViajes/AgenciaDeViajes/Form1.Designer.cs
+++ AgenciaDeViajes/AgenciaDeViajes/Form1.Designer.cs
@@ -53,20 +53,25 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.lblPrecio = new System.Windows.Forms.Label();
             this.btnValidar = new System.Windows.Forms.Button();
-            this.pnlBajo = new System.Windows.Forms.Panel();
-            this.lLblMail = new System.Windows.Forms.LinkLabel();
-            this.pgbCarga = new System.Windows.Forms.ProgressBar();
-            this.lblHora = new System.Windows.Forms.Label();
             this.timHoraActual = new System.Windows.Forms.Timer(this.components);
             this.imgDestinos = new System.Windows.Forms.ImageList(this.components);
             this.pcbLogo = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
             this.menuPrincipal.SuspendLayout();
             this.grbEstancias.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nudPersonas)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudHotel)).BeginInit();
             this.pnlValidar.SuspendLayout();
-            this.pnlBajo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pcbLogo)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnCalcular
@@ -298,43 +303,6 @@
             this.btnValidar.Text = "VALIDAR";
             this.btnValidar.UseVisualStyleBackColor = true;
             // 
-            // pnlBajo
-            // 
-            this.pnlBajo.BackColor = System.Drawing.SystemColors.Highlight;
-            this.pnlBajo.Controls.Add(this.lblHora);
-            this.pnlBajo.Controls.Add(this.pgbCarga);
-            this.pnlBajo.Controls.Add(this.lLblMail);
-            this.pnlBajo.Location = new System.Drawing.Point(0, 733);
-            this.pnlBajo.Name = "pnlBajo";
-            this.pnlBajo.Size = new System.Drawing.Size(877, 27);
-            this.pnlBajo.TabIndex = 14;
-            // 
-            // lLblMail
-            // 
-            this.lLblMail.AutoSize = true;
-            this.lLblMail.Location = new System.Drawing.Point(41, 8);
-            this.lLblMail.Name = "lLblMail";
-            this.lLblMail.Size = new System.Drawing.Size(125, 13);
-            this.lLblMail.TabIndex = 0;
-            this.lLblMail.TabStop = true;
-            this.lLblMail.Text = "vielcontravel@gmail.com";
-            // 
-            // pgbCarga
-            // 
-            this.pgbCarga.Location = new System.Drawing.Point(173, 3);
-            this.pgbCarga.Name = "pgbCarga";
-            this.pgbCarga.Size = new System.Drawing.Size(292, 21);
-            this.pgbCarga.TabIndex = 1;
-            // 
-            // lblHora
-            // 
-            this.lblHora.AutoSize = true;
-            this.lblHora.Location = new System.Drawing.Point(681, 8);
-            this.lblHora.Name = "lblHora";
-            this.lblHora.Size = new System.Drawing.Size(35, 13);
-            this.lblHora.TabIndex = 2;
-            this.lblHora.Text = "label1";
-            // 
             // imgDestinos
             // 
             this.imgDestinos.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgDestinos.ImageStream")));
@@ -355,13 +323,82 @@
             this.pcbLogo.TabIndex = 15;
             this.pcbLogo.TabStop = false;
             // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 738);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(877, 22);
+            this.statusStrip1.TabIndex = 16;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(877, 25);
+            this.toolStrip1.TabIndex = 17;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "toolStripButton3";
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(877, 760);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.pcbLogo);
-            this.Controls.Add(this.pnlBajo);
             this.Controls.Add(this.pnlValidar);
             this.Controls.Add(this.lblHotel);
             this.Controls.Add(this.nudHotel);
@@ -389,9 +426,11 @@
             ((System.ComponentModel.ISupportInitialize)(this.nudHotel)).EndInit();
             this.pnlValidar.ResumeLayout(false);
             this.pnlValidar.PerformLayout();
-            this.pnlBajo.ResumeLayout(false);
-            this.pnlBajo.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pcbLogo)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -422,13 +461,17 @@
         private System.Windows.Forms.Button btnValidar;
         private System.Windows.Forms.Label lblPrecio;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Panel pnlBajo;
-        private System.Windows.Forms.Label lblHora;
-        private System.Windows.Forms.ProgressBar pgbCarga;
-        private System.Windows.Forms.LinkLabel lLblMail;
         private System.Windows.Forms.Timer timHoraActual;
         private System.Windows.Forms.ImageList imgDestinos;
         private System.Windows.Forms.PictureBox pcbLogo;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
     }
 }
 
